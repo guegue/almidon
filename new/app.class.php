f6020f98ce385eecd7812c7f989ee52550e735f6
javier <javier@924a4236-293c-0410-b38f-c0813de1d4fd>
2008-03-28 20:27:18 +0000

    Limpiando...
    git-svn-id: http://svn.almidon.org/trunk@16 924a4236-293c-0410-b38f-c0813de1d4fd

--- new/app.class.php
+++ new/app.class.php
@@ -3,29 +3,23 @@ require_once('config.php');
 require_once('/www/cms/php/db2.class.php');
 require_once('Smarty/Smarty.class.php');
 
-function getArgs() {
-  $params = explode("/", $_SERVER['PATH_INFO']);
-  for($i = 1; $i < sizeof($params); $i++)
-    $args[$i] = $params[$i];
-  return $args;
-}
-
 $smarty = new Smarty;
 $smarty->template_dir = ROOTDIR . '/templates/';
 $smarty->compile_dir = ROOTDIR . '/templates_c/';
 $smarty->config_dir = ROOTDIR . '/configs/';
 $smarty->cache_dir = ROOTDIR . '/cache/';
-                                                                                
-//if ($_SERVER['SERVER_NAME'] == 'secure.guegue.com') {
+
+/*
+# Crea links manualmente                                                                                
+if (ADMIN === true) {
   $adminlinks['pagina.php'] = 'Paginas';
   $adminlinks['categoria.php'] = 'Categorias';
   $adminlinks['enlace.php'] = 'Enlaces';
   $adminlinks['doc.php'] = 'Documentos';
   $adminlinks['galeria.php'] = 'Galeria';
   $smarty->assign('adminlinks', $adminlinks);
-//}
+}
+*/
 
 require('tables.class.php');
 require('extra.class.php');
-?>
-
