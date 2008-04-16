16837ac9f1482fd2f3474d51e81d39fd06c805ad
javier <javier@924a4236-293c-0410-b38f-c0813de1d4fd>
2008-04-16 03:59:22 +0000

    Agregando el demo
    git-svn-id: http://svn.almidon.org/trunk@22 924a4236-293c-0410-b38f-c0813de1d4fd

--- demo/classes/config.php
+++ demo/classes/config.php
@@ -17,7 +17,7 @@ $rootdir = substr($configdir, 0, strrpos($configdir,'/'));
 define ('ROOTDIR', $rootdir);
 define ('DOMAIN', 'demo.almidon.org');
 #define ('ALMIDONDIR', '/usr/local/almidon/');
-define ('ALMIDONDIR', ROOTDIR . '../');
+define ('ALMIDONDIR', ROOTDIR . '/..');
 define ('SQLLOG', ROOTDIR . '/logs/sql.log');
 define ('LOGFILE', ROOTDIR . '/logs/cms.log');
 define ('HOMEDIR', ROOTDIR .'/public_html');
