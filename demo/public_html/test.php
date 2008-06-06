3ccc6a24e31ad62d7d2995b5f4f855e35406e03d
norman <norman@924a4236-293c-0410-b38f-c0813de1d4fd>
2008-06-06 17:12:42 +0000

    Corrigiendo test.php
    git-svn-id: http://svn.almidon.org/trunk@44 924a4236-293c-0410-b38f-c0813de1d4fd

--- demo/public_html/test.php
+++ demo/public_html/test.php
@@ -5,7 +5,7 @@ echo "PHP funciona?" . 5*8 . " = 40<br/>";
 echo "Short tags funcionan" . 5*8 . " = 40<br/>";
 ?>
 <?
-$conn = pg_pconnect("host=localhost port=5432 dbname=almidondemo user=almidondemo password=secreto2");
+$conn = pg_pconnect("host=localhost port=5432 dbname=almidondemo user=almidondemo password=secreto1");
 if ($conn) echo "Conexion a base de datos: OK <br/>";
 $result = pg_query($conn, "SELECT * FROM pagina");
 if ($result) echo "Lectura de datos: OK<br/>";
