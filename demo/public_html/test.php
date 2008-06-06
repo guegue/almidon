69abc761e5354aaf09875234983061ea23d9392f
fitoria <fitoria@924a4236-293c-0410-b38f-c0813de1d4fd>
2008-06-06 18:55:34 +0000

    Agregando user almidonwww al test.php
    git-svn-id: http://svn.almidon.org/trunk@46 924a4236-293c-0410-b38f-c0813de1d4fd

--- demo/public_html/test.php
+++ demo/public_html/test.php
@@ -6,7 +6,9 @@ echo "Short tags funcionan " . 5*8 . " = 40<br/>";
 ?>
 <?
 $conn = pg_pconnect("host=localhost port=5432 dbname=almidondemo user=almidondemo password=secreto1");
-if ($conn) echo "Conexion a base de datos: OK <br/>";
+if ($conn) echo "Conexion a base de datos con usuario almidondemo: OK <br/>";
+$conn2 = pg_pconnect("host=localhost port=5432 dbname=almidondemo user=almidondemowww password=secreto2");
+if ($conn2) echo "Conexion a base de datos con usuario almidondemowww: OK <br/>";
 $result = pg_query($conn, "SELECT * FROM pagina");
 if ($result) echo "Lectura de datos: OK<br/>";
 ?>
