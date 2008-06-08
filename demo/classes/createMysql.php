d61284d461e32ab47f838f8db3cf1d605932e302
fitoria <fitoria@924a4236-293c-0410-b38f-c0813de1d4fd>
2008-06-08 21:29:18 +0000

    agregando createMysql.php (todavia en prueba revisar comentario en lo interno del archivo) updated
    git-svn-id: http://svn.almidon.org/trunk@57 924a4236-293c-0410-b38f-c0813de1d4fd

--- demo/classes/createMysql.php
+++ demo/classes/createMysql.php
@@ -15,12 +15,12 @@ function genSQL($object) {
       $type = 'varchar';
       $size = '500';
     }
-    if ($type == 'order') $type = 'int NULL'; //esta linea se cambio
+    if ($type == 'order') $type = 'int AUTO_INCREMENT'; //esta linea se cambio pero todavia imprime serial enves de int AUTO_INCREMENT no se por que
     if (!$size) $size = $column['size'];
     $size = preg_replace("/\./", ",", $size);
     $sql .= "  ".$column['name']." ".$type;
     if ($size) $sql .= " (".$size.")";
-    if ($column['name'] == $data->key) $sql .= " PRIMARY KEY NOT NULL AUTO_INCREMENT "; //se cambio esta linea para que fuera compatible con MySQL.
+    if ($column['name'] == $data->key) $sql .= " PRIMARY KEY NOT NULL"; //se cambio esta linea para que fuera compatible con MySQL.
     if ($column['references']) $sql .= " REFERENCES ".$column['references'];
     ++$i;
   }
