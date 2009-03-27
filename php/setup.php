75c3ecaa8dd3778c8f7daa1d4e9b2b9b3004282d
javier <javier@924a4236-293c-0410-b38f-c0813de1d4fd>
2009-03-27 19:43:43 +0000

    Automatizar SQL, DD, ERD en setup
    git-svn-id: http://svn.almidon.org/trunk@128 924a4236-293c-0410-b38f-c0813de1d4fd

--- php/setup.php
+++ php/setup.php
@@ -1,6 +1,29 @@
 <?php
-define ('ADMIN', true);
+if (!defined('ADMIN')) define ('ADMIN', true);
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../classes/app.class.php');
+function genLinks($object) {
+  $o = $object . "Table";
+  $data = new $o;
+  if($data->definition)
+  $dd = array();
+  foreach($data->definition as $column) {
+    if ($column['references'] != '0') $dd[] = $column['references'];
+  }
+  return $dd;
+}
+function genDD($object) {
+  $o = $object . "Table";
+  $data = new $o;
+  if($data->definition)
+  $dd = array();
+  foreach($data->definition as $column) {
+    if ($column['size'] == '0') $column['size'] = '&nbsp;';
+    if ($column['references'] == '0') $column['references'] = '&nbsp;';
+    $col = array($column['name'],$column['type'],$column['size'],$column['references'],$column['label']);
+    $dd[] = $col;
+  }
+  return $dd;
+}
 function genSQL($object) {
   $o = $object . "Table";
   $data = new $o;
@@ -10,6 +33,8 @@ function genSQL($object) {
   if($data->definition)
   foreach($data->definition as $column) {
     unset($size);
+    if (!isset($type)) $type = '';
+    if (!isset($size)) $size = '';
     if ($type == 'external') next($data->definition);
     if ($i) $sql .= " ,\n";
     $type = $column['type'];
@@ -40,19 +65,75 @@ function genSQL($object) {
     ++$i;
   }
   $sql .= "\n);\n\n";
-  print $sql;
+  return($sql);
 }
 
-$classes = get_declared_classes();
-foreach($classes as $key) {
-  if (stristr($key, 'table') && $key != 'table' && $key != 'tabledoublekey' && $key != 'Table' && $key != 'TableDoubleKey') {
-    if(substr($key, 0, strpos($key, 'Table')) !== false) $key = substr($key, 0, strpos($key, 'Table'));
-    else $key = substr($key, 0, strpos($key, 'table'));
-    $tables[] = $key;
+$options = array(
+  'sql'=>'Generar SQL basado en tables.class',
+  'dd'=>'Generar diccionario de datos',
+  'erd'=>'Geenrar diagrama entidad relacion');
+$params = explode("/", $_SERVER['REQUEST_URI']);
+$action = $params[3];
+if ($action != 'erd')
+foreach($options as $k=>$option) {
+  if (!isset($params[3])) $prefix = 'setup/';
+  else $prefix = '';
+  print "<a href=\"$prefix$k\">$option</a><br/>";
+}
+if (isset($params[3])) {
+  $classes = get_declared_classes();
+  $output = '';
+  foreach($classes as $key) {
+    if (stristr($key, 'table') && $key != 'table' && $key != 'tabledoublekey' && $key != 'Table' && $key != 'TableDoubleKey') {
+      if(substr($key, 0, strpos($key, 'Table')) !== false) $key = substr($key, 0, strpos($key, 'Table'));
+      else $key = substr($key, 0, strpos($key, 'table'));
+      $tables[] = $key;
+    }
+  }
+  switch ($action) {
+  case 'sql':
+    foreach($tables as $key)
+      $output .= genSQL($key);
+    break;
+  case 'dd':
+    foreach($tables as $key) {
+      $dd = genDD($key);
+      $output .= "\n".'<tr align="center"><td colspan="5" bgcolor="#f0f0f0"><br/>'.$key."</br><br/></td></tr>\n";
+      $output .= "\n<tr><th>Nombre</th><th>Tipo</th><th>Tama&ntilde;o</th><th>Referencias</th><th>Descripci&oacute;n</th></tr>\n";
+      foreach($dd as $col) {
+        $output .= "<tr>";
+        foreach($col as $val)
+          $output .= "<td>$val</td>";
+        $output .= "</tr>\n";
+      }
+    }
+    $output = "\n<table border=\"1\" cellpadding=\"1\" cellspacing=\"1\">$output</table>\n";
+    break;
+  case 'erd':
+    $output = "\ndigraph g {\ngraph [\nrankdir = \"LR\"\n];\nnode [\nfontsize = \"16\"\nshape = \"ellipse\"\n];\nedge [\n];\n";
+    foreach($tables as $key) {
+      $output .= "\"$key\" [\nlabel = \"<f0> $key| <f1>\"\nshape = \"record\"\n];\n";
+      $dd = genLinks($key);
+      $i = 0;
+      foreach($dd as $link) {
+        $output .= "\"$key\":f0 -> \"$link\":f0 [\nid = $i\n];\n";
+        $i++;
+      }
+    }
+    $output .= "}\n";
+    break;
+  }
+  switch($action) {
+  case 'erd':
+    header('Content-type: plain/text');
+    header('Content-Disposition: attachment; filename="erd.dot"');
+    print $output;
+    break;
+  case 'sql':
+    print "<pre>$output</pre>";
+    break;
+  case 'dd':
+    print "$output";
+    break;
   }
 }
-echo '<pre>';
-foreach($tables as $key)
-  genSQL($key);
-echo '</pre>';
-?>
