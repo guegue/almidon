a8925c71d7ea469a5ddf3c5935e012076de68450
christian <christian@924a4236-293c-0410-b38f-c0813de1d4fd>
2009-04-07 23:59:12 +0000

    Correcion de problema de pic, no agarraba el alto (height) de la imagen por que los params de strpos estaban alreves
    git-svn-id: http://svn.almidon.org/trunk@155 924a4236-293c-0410-b38f-c0813de1d4fd

--- phpthumb/pic.php
+++ phpthumb/pic.php
@@ -37,7 +37,7 @@ $args = getArgs();
 if (!isset($args[3])) exit;
 #$_GET['phpThumbDebug'] = true;
 $_GET['src'] = "/../files/" . $args[2] . "/" . $args[3];
-if(strpos('x', $args[1])) {
+if(strpos($args[1],'x')!==false) {
   list($w, $h) = split("x", $args[1]);
 } else {
   $w = $args[1];
