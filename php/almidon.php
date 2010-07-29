fd7e652456e668888beaf8f51615c1353c05bf6e
javier <javier@924a4236-293c-0410-b38f-c0813de1d4fd>
2010-07-29 14:50:21 +0000

    Fix table descriptor
    git-svn-id: http://svn.almidon.org/trunk@329 924a4236-293c-0410-b38f-c0813de1d4fd

--- php/almidon.php
+++ php/almidon.php
@@ -39,7 +39,9 @@ foreach($classes as $key) {
   if (stristr($key, 'table') && $key != 'table' && $key != 'tabledoublekey' && $key != 'Table' && $key != 'TableDoubleKey') {
     $table_object = new $key;
     $global_dd[$table_object->name]['key'] = $table_object->key;
-    if (preg_match('/(^|,)'.$table_object->name.'(,|$)/', $table_object->fields)) {
+    if(isset($table_object->descriptor)) {
+      $global_dd[$table_object->name]['descriptor'] = $table_object->descriptor;
+    } elseif (preg_match('/(^|,)'.$table_object->name.'(,|$)/', $table_object->fields)) {
       $global_dd[$table_object->name]['descriptor'] = $table_object->name;
     } else {
       $global_dd[$table_object->name]['descriptor'] = $table_object->key;
