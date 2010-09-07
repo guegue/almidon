68c0b43bb3081b80edbdb8e9d2e8870f882b4bee
javier <javier@924a4236-293c-0410-b38f-c0813de1d4fd>
2010-09-07 05:16:32 +0000

    Al borrar un registro: Borra archivos relacionados.
    git-svn-id: http://svn.almidon.org/trunk@349 924a4236-293c-0410-b38f-c0813de1d4fd

--- php/db.deleterecord.php
+++ php/db.deleterecord.php
@@ -1,5 +1,16 @@
 <?php
     if (!$id) $id = $this->request[$this->key];
+    # Borra imagenes o archivos relacionados a este registro
+    foreach($this->dd as $key=>$val) {
+      $type = $this->dd[$key]['type'];
+      if ($type == 'image' || $type == 'file' )
+        $remove_files[] = $key;
+    }
+    if (!empty($remove_files)) {
+      $tmp = $this->readRecord($id);
+      foreach($remove_files as $val)
+        unlink(ROOTDIR . '/files/' . $this->name . '/' . $tmp[$val]);
+    }
+    # Borra registro en base de datos
     $sqlcmd = "DELETE FROM $this->name WHERE $this->key = '$id'";
     $result = $this->query($sqlcmd);
-    # FIXME: Borrar imagenes o archivos relacionados a este registro
