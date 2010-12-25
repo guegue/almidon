5a9ac85b9d00cbec2e93833e0d91d3fd7272b1e4
Diógenes Estribí Olivas <destoliv@gmail.com>
2010-12-25 01:39:55 -0600

    Adding new permissions, new instructions to run all scripts as root and deleted the lines that have the words to runuser command and its options, adding the sentence su - postgres (name of administrator) files scripts / setup.sh and demo-scripts / site-setup.sh as key command and then to import *. sql scripts to run as administrator Postgresql Database Server.

--- scripts/phpdoc-almidon.sh
+++ scripts/phpdoc-almidon.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-d ../core/
-phpdoc -t ../doc/phpdoc/ -f php/db2.class.php,php/db.dal.php,php/db.const.php,../demo/classes/config.ori.php,php/almidon.php
+
+cuser="`whoami`"
+
+#Sólo ejecuta correctamente este script sólo si el usuario quien lo manda a ejecutar
+#es el usuario root, quien es el nombre del administrador del sistema.
+if [ $cuser = "root" ];then
+ cd ../core/
+ phpdoc -t ../doc/phpdoc/ -f php/db2.class.php,php/db.dal.php,php/db.const.php,../demo/classes/config.ori.php,php/almidon.php
+else
+    echo  "Must be the administrator named root for execute this script without any problem";
+    echo "Try again with as administrator";
+fi
