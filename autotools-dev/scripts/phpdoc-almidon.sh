96fd545fc6d839ea0b49ead689ac0ee14e2a3821
Javier Wilson <javier@guegue.net>
2011-01-13 01:23:39 +0100

    Revert "added autotools-dev"
    This reverts commit 1bd03bedf3efa63d7bad5ade68005ab2f833bc6a.

--- autotools-dev/scripts/phpdoc-almidon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-template_website_almidon_demodir = ${datarootdir}/almidon/demo
-template_almidon_doc_phpdocdir  = ${docdir}/almidon/phpdoc
-
-phpdoc -t $almidon_doc_phpdocdir/ -f php/db2.class.php, php/db.dal.php, php/db.const.php,$template_website_almidon_demodir/classes/config.ori.php
