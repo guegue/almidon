9b4adda331df257c513c6e8e076a72bb95f8a868
Javier Wilson <javier@guegue.net>
2011-02-10 22:42:03 +0100

    fix to allow id_key (when no alm_old_id_key)

--- core/php/db.updaterecord.php
+++ core/php/db.updaterecord.php
@@ -4,7 +4,8 @@
     if (isset($id) && !is_array($id)) $id = array($id); //old-fashioned way readRecord(int);
     if (!isset($id)) foreach($this->keys as $key=>$val) $id[] = null;
     foreach($id as $key=>$val) {
-      if(empty($val)) $val = $this->request['alm_old_' . $this->keys[$key]];
+      if(empty($val))
+        $val = (isset($this->request['alm_old_' . $this->keys[$key]])) ? $this->request['alm_old_' . $this->keys[$key]] : $this->request[$this->keys[$key]];
       $keyfilter[] = $this->keys[$key] . " = '$val'";
     }
     $filter = join(' AND ', $keyfilter);
