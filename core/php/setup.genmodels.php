cc02a41f20e964d279fc131eac9b2332f81f6638
Javier Wilson <javier.wilson@gmail.com>
2012-05-14 02:35:42 +0100

    mejorando alm2django

--- core/php/setup.genmodels.php
+++ core/php/setup.genmodels.php
@@ -2,25 +2,27 @@
 function genColumnModel($column, $dbtype) {
   $sql = '';
   $type = $column['type'];
+  $name = $column['name'];
   if ($type == 'external') return;
   if ($type == 'automatic') {
     switch($column['extra']['automatic']) {
     case 'now':
-      $type = 'timestamp';
+      $type = 'DateTimeField';
       $size = null;
       break;
     default:
       $type = 'CharField';
       $size = '32';
     }
-  } elseif ($type == 'file' || $type == 'image' || $type == 'autoimage') {
-    $type = 'CharField';
-    $size = '500';
+  } elseif ($type == 'image' || $type == 'autoimage') {
+    $type = 'ImageField';
+  } elseif ($type == 'file') {
+    $type = 'FileField';
   } elseif ($type == 'html' || $type == 'xhtml' || $type == 'text') {
     $type = 'TextField';
     $size = null;
   } elseif ($type == 'datetime') {
-    $type = 'timestamp';
+    $type = 'DateTimeField';
     $size = null;
   } elseif ($type == 'datenull') {
     $type = 'DateField';
@@ -29,6 +31,8 @@ function genColumnModel($column, $dbtype) {
     $type = 'CharField';
   } elseif ($type == 'varchar') {
     $type = 'CharField';
+    if ($name == 'email') $type = 'EmailField';
+    if ($name == 'url') $type = 'UrlField';
   } elseif ($type == 'int') {
     $type = 'IntegerField';
   } elseif ($type == 'date') {
@@ -44,10 +48,17 @@ function genColumnModel($column, $dbtype) {
   }
   $size = isset($size) ? $size : $column['size'];
   $size = preg_replace("/\./", ",", $size);
-  $model .= $column['name']." = models.".$type;
-  if ($size) $model .= " (max_length=".$size.")";
-  if ($column['pk']) $model .= " (primary_key=True)";
-  if ($column['references']) $sql .= " REFERENCES ".$column['references'];
+  if ($column['references']) {
+    $name = preg_replace('/^id/', '', $name);
+    $model = "$name = ForeignKey";
+    $params[] = $column['references'];
+  } else {
+    $model = "$name = models.".$type;
+    if ($size) $params[] = "max_length=$size";
+    if ($column['pk']) $params[] = "primary_key=True";
+  }
+  if ($column['label']) $params[] = 'verbose_name="' . $column['label'] . '"';
+  if ($params) $model .= '(' . join(', ', $params) . ')';
   return $model;
 }
 function genModel($object) {
@@ -65,7 +76,12 @@ function genModel($object) {
     $model .= "    " . genColumnModel($column, $dbtype);
     ++$i;
   }
-  $sql .= ', PRIMARY KEY ( '. join(',',$data->keys) .' ) ';
+  $model .= "\n    Class Meta:\n";
+  $model .= "        db_table = '$object'\n";
+  if ($data->order) {
+    $order = preg_replace('/(.*) DESC/', '-$1', $data->order);
+    $model .= "        ordering = ['" . $order . "']\n";
+  }
   $model .= "\n\n";
   return($model);
 }
